d13595d1aea90e6ef7648af6cb3fcb2de35b68db
SaiVatsal <vatsal1327@gmail.com>
2026-04-16 23:32:43 +0530

    3488-closest-equal-element-queries .py Medium 04-16-2026

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -7,7 +7,6 @@ class Solution:
         for i, val in enumerate(nums):
             pos[val].append(i) 
         min_dist = [-1] * n
-        
         for indices in pos.values():
             m = len(indices)
             if m > 1:
@@ -15,13 +14,9 @@ class Solution:
                     curr = indices[i]
                     prev_idx = indices[i - 1]
                     next_idx = indices[(i + 1) % m]
-                    
                     d1 = abs(curr - prev_idx)
                     d1 = min(d1, n - d1)
-                    
                     d2 = abs(curr - next_idx)
                     d2 = min(d2, n - d2)
-                    
                     min_dist[curr] = min(d1, d2)
-                    
         return [min_dist[q] for q in queries]

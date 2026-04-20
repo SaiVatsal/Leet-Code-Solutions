7d15e759b89a1426b365db39b5bc68a4561b57a9
SaiVatsal <vatsal1327@gmail.com>
2026-04-20 20:11:01 +0530

    2078-two-furthest-houses-with-different-colors .py Easy 04-20-2026

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -6,6 +6,6 @@ class Solution:
 
         while colors[0] == colors[j]:
             j -= 1 
-        while colours[-1] == colors[i]:
+        while colors[-1] == colors[i]:
             i +=1
         return max(j, n - 1 - i)

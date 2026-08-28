6a5c0cc962bcb795ee5dd329047145f0975f2597
SaiVatsal <vatsal1327@gmail.com>
2026-08-28 16:52:34 +0530

    Refactor trapping rain water solution in C++

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,40 +1,42 @@
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
 
-using namespace std;
+int trap(int* height, int heightSize) {
+    if (heightSize == 0)
+        return 0;
 
-class Solution {
-public:
-    int trap(vector<int>& height) {
-            if (height.empty()) return 0;
+    int left = 0;
+    
+    int right = heightSize - 1;
+    
+    int left_max = 0;
+    
+    int right_max = 0;
+    
+    int water_trapped = 0;
+// length of right left max and min trapped water according to image
+    while (left < right) {
+        
+        if (height[left] < height[right]) {
 
-                    int left = 0;
-                            int right = height.size() - 1;
-                                    int left_max = 0;
-                                            int right_max = 0;
-                                                    int water_trapped = 0;
+            if (height[left] >= left_max) {
+                left_max = height[left];
+            } else {
+                water_trapped += left_max - height[left];
+            }
 
-                                                            while (left < right) {
-                                                                        // We always process the side with the shorter height 
-                                                                                    // because that side is the "bottleneck" for trapping water.
-                                                                                                if (height[left] < height[right]) {
-                                                                                                                if (height[left] >= left_max) {
-                                                                                                                                    left_max = height[left];
-                                                                                                                                                    } else {
-                                                                                                                                                                        water_trapped += (left_max - height[left]);
-                                                                                                                                                                                        }
-                                                                                                                                                                                                        left++;
-                                                                                                                                                                                                                    } else {
-                                                                                                                                                                                                                                    if (height[right] >= right_max) {
-                                                                                                                                                                                                                                                        right_max = height[right];
-                                                                                                                                                                                                                                                                        } else {
-                                                                                                                                                                                                                                                                                            water_trapped += (right_max - height[right]);
-                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                            right--;
-                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                }
+            left++;
+// increament 
+        } else {
 
-                                                                                                                                                                                                                                                                                                                                                        return water_trapped;
-                                                                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                                                            };
-                                                                                                                                                                                                                                                                                                                                                            
+            if (height[right] >= right_max) {
+                right_max = height[right];
+            } else {
+                water_trapped += right_max - height[right];
+            }
+
+            right--;
+        }
+    }
+
+    return water_trapped;
+}
